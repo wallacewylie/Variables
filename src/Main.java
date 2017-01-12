60265ad782e75eb41ee1efa944bf766413250ea3
Wallace Wylie <wallace.wylie@gmail.com>
2017-01-12 13:28:11 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int myVar;
+
+	myVar = 50;
+	System.out.println(myVar);
+
+	int anotherVar = 100;
+    System.out.println(anotherVar);
+
+    myVar = anotherVar;
+    System.out.println(myVar);
+
+    anotherVar = 200;
+    System.out.println(anotherVar);
+
+    System.out.println(myVar);
+    }
+}
